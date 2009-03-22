b93d954a751d8bd22ea41ee4713482aea592c94c
Martin Grotzke <martin.grotzke@javakaffee.de>
2009-03-22 22:50:01 +0100

    Fix Issue 1: Fix license headers, See http://code.google.com/p/memcached-session-manager/issues/detail?id=1

--- src/test/java/de/javakaffee/web/msm/LRUCacheTest.java
+++ src/test/java/de/javakaffee/web/msm/LRUCacheTest.java
@@ -1,13 +1,18 @@
 /*
- * $Id: $ (c)
- * Copyright 2009 freiheit.com technologies GmbH
+ * Copyright 2009 Martin Grotzke
  *
- * Created on Mar 15, 2009
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * This file contains unpublished, proprietary trade secret information of
- * freiheit.com technologies GmbH. Use, transcription, duplication and
- * modification are strictly prohibited without prior written consent of
- * freiheit.com technologies GmbH.
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an &quot;AS IS&quot; BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
  */
 package de.javakaffee.web.msm;
 
