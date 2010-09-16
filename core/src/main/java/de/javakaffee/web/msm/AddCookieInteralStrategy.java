f584a8b41c36151e36a8cfe6d6b8eb53b9a44c0b
Martin Grotzke <martin.grotzke@javakaffee.de>
2010-09-16 03:58:08 +0200

    Make AddCookieInteralStrategy compatible with tomcat < 6.0.19, >= 6.0.19 and 7.0.x - only for tomcat >= 6.0.19 and < 7.0.x Response.addCookieInternal(Cookie, boolean) is available.

--- core/src/main/java/de/javakaffee/web/msm/AddCookieInteralStrategy.java
+++ core/src/main/java/de/javakaffee/web/msm/AddCookieInteralStrategy.java
@@ -26,10 +26,11 @@ import org.apache.catalina.connector.Response;
 /**
  * This class abstracts the invocation of setting the session cookie on the
  * {@link Response}, which can include the HttpOnly flag configured at
- * the {@link Context} ({@link Context#setUseHttpOnly(boolean)}) for tomcat >= 6.0.19
+ * the {@link Context} ({@link Context#setUseHttpOnly(boolean)}) for tomcat >= 6.0.19 and < 7.0.x
  * (via {@link Response#addCookieInternal(Cookie, boolean)})
  * or just invokes {@link Response#addCookieInternal(Cookie)} if the HttpOnly flag
- * is not yet supported (for previous tomcat versions).
+ * is not yet supported (for previous tomcat versions) or if HttpOnly flag is supported
+ * but the Cookie already provides <code>isHttpOnly</code>.
  * <p>
  * This was introduced for feature request
  * #54: "Support session cookie 'HttpOnly' flag when session id is rewritten due to memcached failover"
@@ -45,14 +46,22 @@ abstract class AddCookieInteralStrategy {
         } catch ( final SecurityException e ) {
             throw new RuntimeException( e );
         } catch ( final NoSuchMethodException e ) {
-            return new LegacyAddCookieInteralStrategy();
+            return new StandardAddCookieInteralStrategy();
         }
     }
 
     private static AddCookieInteralStrategy createUseHttpOnlyStrategy( final Method getUseHttpOnly, final Context context ) {
+        // we have Context.getUseHttpOnly (which was introduced in 6.0.19) and want to set the cookie via
+        // Response.addCookieInternal(Cookie, boolean), which was removed in 7.0.x
+        // as the Servlet Spec 3.0 added Cookie.isHttpOnly so that Response.addCookieInternal(Cookie) is sufficient again
         try {
+            // test for Response.addCookieInternal(Cookie, boolean) or get a NoSuchMethodException...
+            final Method addCookieInternal = Response.class.getMethod( "addCookieInternal", Cookie.class, boolean.class );
             final boolean useHttpOnly = ( (Boolean) getUseHttpOnly.invoke( context ) ).booleanValue();
-            return new AddCookieInteralStrategyUseHttpOnly( useHttpOnly );
+            return new AddCookieInteralStrategyUseHttpOnly( addCookieInternal, useHttpOnly );
+        } catch ( final NoSuchMethodException e ) {
+            // for tomcat7 we can use standard strategy
+            return new StandardAddCookieInteralStrategy();
         } catch ( final Exception e ) {
             throw new RuntimeException( "Caught unexpected exception while invoking context.getUseHttpOnly.", e );
         }
@@ -67,9 +76,9 @@ abstract class AddCookieInteralStrategy {
     abstract void addCookieInternal( Cookie newCookie, Response response );
 
     /**
-     * The AddCookieInteralStrategy for tomcat < 6.0.19.
+     * The AddCookieInteralStrategy for tomcat < 6.0.19 and tomcat >= 7.0.x
      */
-    private static final class LegacyAddCookieInteralStrategy extends AddCookieInteralStrategy {
+    private static final class StandardAddCookieInteralStrategy extends AddCookieInteralStrategy {
 
         /**
          * {@inheritDoc}
@@ -82,14 +91,16 @@ abstract class AddCookieInteralStrategy {
     }
 
     /**
-     * The AddCookieInteralStrategy for tomcat >= 6.0.19 with support for
+     * The AddCookieInteralStrategy for tomcat >= 6.0.19 and < 7.0.x with support for
      * {@link Response#addCookieInternal(Cookie, boolean)}.
      */
     private static final class AddCookieInteralStrategyUseHttpOnly extends AddCookieInteralStrategy {
 
+        private final Method _addCookieInternal;
         private final boolean _useHttpOnly;
 
-        AddCookieInteralStrategyUseHttpOnly( final boolean useHttpOnly ) {
+        AddCookieInteralStrategyUseHttpOnly( final Method addCookieInternal, final boolean useHttpOnly ) {
+            _addCookieInternal = addCookieInternal;
             _useHttpOnly = useHttpOnly;
         }
 
@@ -98,7 +109,12 @@ abstract class AddCookieInteralStrategy {
          */
         @Override
         void addCookieInternal( final Cookie newCookie, final Response response ) {
-            response.addCookieInternal( newCookie );
+            try {
+                _addCookieInternal.invoke( response, newCookie, _useHttpOnly );
+            } catch ( final Exception e ) {
+                throw new RuntimeException( "Caught exception when trying to invoke" +
+                		" Response.addCookieInternal(Cookie, boolean).", e );
+            }
         }
 
     }
