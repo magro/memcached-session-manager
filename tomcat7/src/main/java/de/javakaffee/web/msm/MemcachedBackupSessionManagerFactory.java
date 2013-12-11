58ba4c4b32a29731c065d0ddb475c7a2ddf41b2d
Joshua Chaitin-Pollak <josh@offthehill.org>
2013-12-10 23:19:24 -0600

    Constructed a factory for use with the tomcat-maven-plugin standalone war functionality. This factory can be called from the tomcat standalone bootstrap code to use memcached-session-manager within your standalone project. I only implemented it for TC7 because the standalone war support is only for that version of tomcat. When using this factory, you configure msm with system properties. I could imagine this being improved with models.
    Using this patch, in conjunction with the tomcat-maven-plugin patch, you can start your standalone project like this:
    
    java -Dmsm.memcachedNodes="n1:localhost:21211" -jar standalone.jar -sessionManagerFactory de.javakaffee.web.msm.MemcachedBackupSessionManagerFactory

---
+++ tomcat7/src/main/java/de/javakaffee/web/msm/MemcachedBackupSessionManagerFactory.java
@@ -0,0 +1,51 @@
+package de.javakaffee.web.msm;
+
+import org.apache.catalina.Manager;
+
+import de.javakaffee.web.msm.MemcachedSessionService.SessionManager;
+
+public class MemcachedBackupSessionManagerFactory {
+
+    private static final String DEFAULT_TRANSCODER_FACTORY = JavaSerializationTranscoderFactory.class.getName();
+    
+    public Manager createSessionManager() {
+        SessionManager sessionManager = new MemcachedBackupSessionManager();
+        
+        String memcachedNodes = System.getProperty("msm.memcachedNodes");
+        String failoverNodes = System.getProperty("msm.failoverNodes", "");
+        boolean enabled = Boolean.parseBoolean(System.getProperty("msm.enabled", "true"));
+        boolean sticky = Boolean.parseBoolean(System.getProperty("msm.sticky", "true"));;
+        String lockingMode = System.getProperty("msm.lockingMode", null);
+        String memcachedProtocol =  System.getProperty("msm.memcachedProtocol", MemcachedSessionService.PROTOCOL_TEXT);
+        String username = System.getProperty("msm.username", "");
+        
+        // Default: One-Hour timeout
+        int sessionTimeout = Integer.parseInt(System.getProperty("msm.sessionTimeout", "3600"));
+        String transcoderFactoryClassName = System.getProperty("msm.failoverNodes", DEFAULT_TRANSCODER_FACTORY);
+        
+        // Example: ".*\\.(png|gif|jpg|css|js|ico)$"
+        String requestUriIgnorePattern = System.getProperty("msm.requestUriIgnorePattern", "");
+        
+        /* we must set the maxInactiveInterval after the context,
+         * as setContainer(context) uses the session timeout set on the context
+         */
+        sessionManager.getMemcachedSessionService().setMemcachedNodes( memcachedNodes );
+        sessionManager.getMemcachedSessionService().setFailoverNodes( failoverNodes );
+        sessionManager.getMemcachedSessionService().setEnabled(enabled);
+        sessionManager.getMemcachedSessionService().setSticky(sticky);
+        if(lockingMode != null) {
+            sessionManager.getMemcachedSessionService().setLockingMode(lockingMode);
+        }
+        sessionManager.getMemcachedSessionService().setMemcachedProtocol(memcachedProtocol);
+        sessionManager.getMemcachedSessionService().setUsername(username);
+        sessionManager.setMaxInactiveInterval( sessionTimeout ); // 1 second
+        sessionManager.getMemcachedSessionService().setSessionBackupAsync( false );
+        sessionManager.getMemcachedSessionService().setSessionBackupTimeout( 100 );
+        sessionManager.setProcessExpiresFrequency( 1 ); // 1 second (factor for context.setBackgroundProcessorDelay)
+        sessionManager.getMemcachedSessionService().setTranscoderFactoryClass( transcoderFactoryClassName);
+        sessionManager.getMemcachedSessionService().setRequestUriIgnorePattern(requestUriIgnorePattern);
+
+        System.out.println("MemcachedBackupSessionManager constructed");
+        return sessionManager;
+    }
+}
