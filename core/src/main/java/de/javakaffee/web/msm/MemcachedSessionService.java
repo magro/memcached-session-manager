magro/memcached-session-manager

commit eddf4c45c7a224516867a1b59f6610177a389ce1

Revert "Use Context.sessionTimeout to set timeout for new sessions."
And manage the maxInactiveInterval in our own manager instead.

This is preferred, because the change broke tests, that used maxInactiveInterval
to set fine grained session timeouts. If these tests now would have to
use Context.sessionTimeout the minimum session timeout would be 60 seconds,
which would increase the test time significantly.

Also this fixes tests that were broken by the usage of Manager.getContext instead
Manager.getContainer (commit 390de3dd) and the upgrade of tomcat versions
(commit e1f3c07d), that introduced Manager.willAttributeDistribute that needs
to be mocked in some tests.

This reverts commit 1e7b2873fcd2b36839464a68f969ea5455d0dc2a.

Should finally resolve #48 and resolve #287

core/src/main/java/de/javakaffee/web/msm/MemcachedSessionService.java
@@ -296,6 +296,9 @@ public class MemcachedSessionService {
          */
         String getString(final String key, final Object... args);
 
+        int getMaxInactiveInterval();
+        void setMaxInactiveInterval(int interval);
+
         int getMaxActiveSessions();
         void incrementSessionCounter();
         void incrementRejectedSessions();
@@ -677,7 +680,7 @@ public class MemcachedSessionService {
         session.setNew( true );
         session.setValid( true );
         session.setCreationTime( System.currentTimeMillis() );
-        session.setMaxInactiveInterval( _manager.getContext().getSessionTimeout() * 60 );
+        session.setMaxInactiveInterval( _manager.getMaxInactiveInterval() );
 
         if ( sessionId == null || !_memcachedNodesManager.canHitMemcached( sessionId ) ) {
             sessionId = _manager.generateSessionId();

tomcat6/src/main/java/de/javakaffee/web/msm/MemcachedBackupSessionManager.java
@@ -76,7 +76,7 @@ import de.javakaffee.web.msm.LockingStrategy.LockingMode;
  * @author <a href="mailto:martin.grotzke@javakaffee.de">Martin Grotzke</a>
  * @version $Id$
  */
-public class MemcachedBackupSessionManager extends ManagerBase implements Lifecycle, MemcachedSessionService.SessionManager {
+public class MemcachedBackupSessionManager extends ManagerBase implements Lifecycle, PropertyChangeListener, MemcachedSessionService.SessionManager {
 
     protected static final String NAME = MemcachedBackupSessionManager.class.getSimpleName();
 
@@ -85,6 +85,13 @@ public class MemcachedBackupSessionManager extends ManagerBase implements Lifecy
     protected final Log _log = LogFactory.getLog( getClass() );
 
     private final LifecycleSupport _lifecycle = new LifecycleSupport( this );
+
+    /**
+     * The default maximum inactive interval for Sessions created by
+     * this Manager.
+     * Used instead of Context.sessionTimeout to allow tests to set more fine grained session timeouts.
+     */
+    private int _maxInactiveInterval = 30 * 60;
 
     private int _maxActiveSessions = -1;
     private int _rejectedSessions;
@@ -140,6 +147,28 @@ public class MemcachedBackupSessionManager extends ManagerBase implements Lifecy
     protected void startInternal( final MemcachedClient memcachedClient ) throws LifecycleException {
         _msm.setMemcachedClient(memcachedClient);
         _msm.startInternal();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void setContainer( final Container container ) {
+
+        // De-register from the old Container (if any)
+        if ( this.container != null && this.container instanceof Context ) {
+            ( (Context) this.container ).removePropertyChangeListener( this );
+        }
+
+        // Default processing provided by our superclass
+        super.setContainer( container );
+
+        // Register with the new Container (if any)
+        if ( this.container != null && this.container instanceof Context ) {
+            setMaxInactiveInterval( ( (Context) this.container ).getSessionTimeout() * 60 );
+            ( (Context) this.container ).addPropertyChangeListener( this );
+        }
+
     }
 
     /**
@@ -235,6 +264,18 @@ public class MemcachedBackupSessionManager extends ManagerBase implements Lifecy
         // so that session backup won't be omitted we must store this event
         super.changeSessionId( session );
         ((MemcachedBackupSession)session).setSessionIdChanged( true );
+    }
+
+    public int getMaxInactiveInterval() {
+        return _maxInactiveInterval;
+    }
+
+    public void setMaxInactiveInterval(int interval) {
+        int oldMaxInactiveInterval = _maxInactiveInterval;
+        _maxInactiveInterval = interval;
+        support.firePropertyChange("maxInactiveInterval",
+                                   Integer.valueOf(oldMaxInactiveInterval),
+                                   Integer.valueOf(_maxInactiveInterval));
     }
 
     /**
@@ -686,6 +727,28 @@ public class MemcachedBackupSessionManager extends ManagerBase implements Lifecy
     public void backgroundProcess() {
         _msm.updateExpirationInMemcached();
         super.backgroundProcess();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void propertyChange( final PropertyChangeEvent event ) {
+
+        // Validate the source of this event
+        if ( !( event.getSource() instanceof Context ) ) {
+            return;
+        }
+
+        // Process a relevant property change
+        if ( event.getPropertyName().equals( "sessionTimeout" ) ) {
+            try {
+                setMaxInactiveInterval( ( (Integer) event.getNewValue() ).intValue() * 60 );
+            } catch ( final NumberFormatException e ) {
+                _log.warn( "standardManager.sessionTimeout: " + event.getNewValue().toString() );
+            }
+        }
+
     }
 
     /**

tomcat7/src/main/java/de/javakaffee/web/msm/MemcachedBackupSessionManager.java
@@ -83,6 +83,13 @@ public class MemcachedBackupSessionManager extends ManagerBase implements Lifecy
 
     protected MemcachedSessionService _msm;
 
+    /**
+     * The default maximum inactive interval for Sessions created by
+     * this Manager.
+     * Used instead of Context.sessionTimeout to allow tests to set more fine grained session timeouts.
+     */
+    private int _maxInactiveInterval = 30 * 60;
+
     private Boolean _contextHasFormBasedSecurityConstraint;
 
     public MemcachedBackupSessionManager() {
@@ -224,6 +231,18 @@ public class MemcachedBackupSessionManager extends ManagerBase implements Lifecy
         // so that session backup won't be omitted we must store this event
         super.changeSessionId( session );
         ((MemcachedBackupSession)session).setSessionIdChanged( true );
+    }
+
+    public int getMaxInactiveInterval() {
+        return _maxInactiveInterval;
+    }
+
+    public void setMaxInactiveInterval(int interval) {
+        int oldMaxInactiveInterval = _maxInactiveInterval;
+        _maxInactiveInterval = interval;
+        support.firePropertyChange("maxInactiveInterval",
+                Integer.valueOf(oldMaxInactiveInterval),
+                Integer.valueOf(_maxInactiveInterval));
     }
 
     /**

tomcat8/src/main/java/de/javakaffee/web/msm/MemcachedBackupSessionManager.java
@@ -78,6 +78,13 @@ public class MemcachedBackupSessionManager extends ManagerBase implements Lifecy
 
     protected MemcachedSessionService _msm;
 
+    /**
+     * The default maximum inactive interval for Sessions created by
+     * this Manager.
+     * Used instead of Context.sessionTimeout to allow tests to set more fine grained session timeouts.
+     */
+    private int _maxInactiveInterval = 30 * 60;
+
     private Boolean _contextHasFormBasedSecurityConstraint;
 
     public MemcachedBackupSessionManager() {
@@ -207,6 +214,18 @@ public class MemcachedBackupSessionManager extends ManagerBase implements Lifecy
         // so that session backup won't be omitted we must store this event
         super.changeSessionId( session );
         ((MemcachedBackupSession)session).setSessionIdChanged(true);
+    }
+
+    public int getMaxInactiveInterval() {
+        return _maxInactiveInterval;
+    }
+
+    public void setMaxInactiveInterval(int interval) {
+        int oldMaxInactiveInterval = _maxInactiveInterval;
+        _maxInactiveInterval = interval;
+        support.firePropertyChange("maxInactiveInterval",
+                Integer.valueOf(oldMaxInactiveInterval),
+                Integer.valueOf(_maxInactiveInterval));
     }
 
     /**

core/src/test/java/de/javakaffee/web/msm/TranscoderServiceTest.java
@@ -33,6 +33,7 @@ import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.catalina.Context;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.authenticator.Constants;
 import org.apache.catalina.authenticator.SavedRequest;
@@ -61,7 +62,9 @@ public abstract class TranscoderServiceTest {
 
         _manager = mock( SessionManager.class );
 
-        when( _manager.getContext() ).thenReturn( new StandardContext() ); // needed for createSession
+        final Context context = new StandardContext();
+        when( _manager.getContext() ).thenReturn( context ); // needed for createSession
+        when( _manager.getContainer() ).thenReturn( context ); // needed for createSession
         when( _manager.newMemcachedBackupSession() ).thenAnswer(new Answer<MemcachedBackupSession>() {
             @Override
             public MemcachedBackupSession answer(final InvocationOnMock invocation) throws Throwable {
@@ -79,6 +82,7 @@ public abstract class TranscoderServiceTest {
 
         when( _manager.readPrincipal( (ObjectInputStream)any() ) ).thenReturn( createPrincipal() );
         when( _manager.getMemcachedSessionService() ).thenReturn( service );
+        when( _manager.willAttributeDistribute(anyString(), any())).thenReturn(true);
 
     }
 

core/src/test/java/de/javakaffee/web/msm/serializer/hibernate/AbstractHibernateCollectionsTest.java
@@ -31,6 +31,7 @@ import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.criterion.Restrictions;
+import org.mockito.Matchers;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -44,6 +45,8 @@ import java.util.List;
 import java.util.Set;
 
 import static de.javakaffee.web.msm.integration.TestUtils.createContext;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.anyObject;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -208,9 +211,12 @@ public abstract class AbstractHibernateCollectionsTest {
     @Nonnull
     protected SessionManager createSessionManager() {
         final SessionManager manager = mock( SessionManager.class );
-        when( manager.getContext() ).thenReturn( new StandardContext() ); // needed for createSession
+        final StandardContext context = new StandardContext();
+        when( manager.getContext() ).thenReturn(context); // needed for createSession
+        when( manager.getContainer() ).thenReturn(context); // needed for createSession
         when( manager.getMemcachedSessionService() ).thenReturn(newMemcachedSessionService(manager));
         when( manager.newMemcachedBackupSession() ).thenReturn( new MemcachedBackupSession( manager ) );
+        when( manager.willAttributeDistribute(anyString(), anyObject())).thenReturn(true);
         return manager;
     }
 

commit 2befaa5b728433b745d95e20d9071cec01475812

Use context session timeout
This bug was introduced with commit eddf4c45 (Revert "Use
Context.sessionTimeout to set timeout for new sessions.") because
tomcat no longer invoked the Manager.setMaxInactiveInterval. This
commit hard coded the session timeout without considering the one
set on the Context.

Refs #48, refs #287

core/src/main/java/de/javakaffee/web/msm/MemcachedSessionService.java
@@ -296,6 +296,7 @@ public class MemcachedSessionService {
          */
         String getString(final String key, final Object... args);
 
+        boolean isMaxInactiveIntervalSet();
         int getMaxInactiveInterval();
         void setMaxInactiveInterval(int interval);
 
@@ -680,7 +681,9 @@ public class MemcachedSessionService {
         session.setNew( true );
         session.setValid( true );
         session.setCreationTime( System.currentTimeMillis() );
-        session.setMaxInactiveInterval( _manager.getMaxInactiveInterval() );
+        session.setMaxInactiveInterval( _manager.isMaxInactiveIntervalSet()
+                ? _manager.getMaxInactiveInterval()
+                : _manager.getContext().getSessionTimeout() * 60 );
 
         if ( sessionId == null || !_memcachedNodesManager.canHitMemcached( sessionId ) ) {
             sessionId = _manager.generateSessionId();

tomcat6/src/main/java/de/javakaffee/web/msm/MemcachedBackupSessionManager.java
@@ -86,12 +86,8 @@ public class MemcachedBackupSessionManager extends ManagerBase implements Lifecy
 
     private final LifecycleSupport _lifecycle = new LifecycleSupport( this );
 
-    /**
-     * The default maximum inactive interval for Sessions created by
-     * this Manager.
-     * Used instead of Context.sessionTimeout to allow tests to set more fine grained session timeouts.
-     */
-    private int _maxInactiveInterval = 30 * 60;
+    /** Can be used to override Context.sessionTimeout (to allow tests to set more fine grained session timeouts) */
+    private Integer _maxInactiveInterval;
 
     private int _maxActiveSessions = -1;
     private int _rejectedSessions;
@@ -266,16 +262,21 @@ public class MemcachedBackupSessionManager extends ManagerBase implements Lifecy
         ((MemcachedBackupSession)session).setSessionIdChanged( true );
     }
 
+    @Override
+    public boolean isMaxInactiveIntervalSet() {
+        return _maxInactiveInterval != null;
+    }
+
     public int getMaxInactiveInterval() {
         return _maxInactiveInterval;
     }
 
     public void setMaxInactiveInterval(int interval) {
-        int oldMaxInactiveInterval = _maxInactiveInterval;
+        Integer oldMaxInactiveInterval = _maxInactiveInterval;
         _maxInactiveInterval = interval;
         support.firePropertyChange("maxInactiveInterval",
-                                   Integer.valueOf(oldMaxInactiveInterval),
-                                   Integer.valueOf(_maxInactiveInterval));
+                oldMaxInactiveInterval,
+                _maxInactiveInterval);
     }
 
     /**

tomcat7/src/main/java/de/javakaffee/web/msm/MemcachedBackupSessionManager.java
@@ -83,12 +83,8 @@ public class MemcachedBackupSessionManager extends ManagerBase implements Lifecy
 
     protected MemcachedSessionService _msm;
 
-    /**
-     * The default maximum inactive interval for Sessions created by
-     * this Manager.
-     * Used instead of Context.sessionTimeout to allow tests to set more fine grained session timeouts.
-     */
-    private int _maxInactiveInterval = 30 * 60;
+    /** Can be used to override Context.sessionTimeout (to allow tests to set more fine grained session timeouts) */
+    private Integer _maxInactiveInterval;
 
     private Boolean _contextHasFormBasedSecurityConstraint;
 
@@ -233,16 +229,21 @@ public class MemcachedBackupSessionManager extends ManagerBase implements Lifecy
         ((MemcachedBackupSession)session).setSessionIdChanged( true );
     }
 
+    @Override
+    public boolean isMaxInactiveIntervalSet() {
+        return _maxInactiveInterval != null;
+    }
+
     public int getMaxInactiveInterval() {
         return _maxInactiveInterval;
     }
 
     public void setMaxInactiveInterval(int interval) {
-        int oldMaxInactiveInterval = _maxInactiveInterval;
+        Integer oldMaxInactiveInterval = _maxInactiveInterval;
         _maxInactiveInterval = interval;
         support.firePropertyChange("maxInactiveInterval",
-                Integer.valueOf(oldMaxInactiveInterval),
-                Integer.valueOf(_maxInactiveInterval));
+                oldMaxInactiveInterval,
+                _maxInactiveInterval);
     }
 
     /**

tomcat8/src/main/java/de/javakaffee/web/msm/MemcachedBackupSessionManager.java
@@ -78,12 +78,8 @@ public class MemcachedBackupSessionManager extends ManagerBase implements Lifecy
 
     protected MemcachedSessionService _msm;
 
-    /**
-     * The default maximum inactive interval for Sessions created by
-     * this Manager.
-     * Used instead of Context.sessionTimeout to allow tests to set more fine grained session timeouts.
-     */
-    private int _maxInactiveInterval = 30 * 60;
+    /** Can be used to override Context.sessionTimeout (to allow tests to set more fine grained session timeouts) */
+    private Integer _maxInactiveInterval;
 
     private Boolean _contextHasFormBasedSecurityConstraint;
 
@@ -216,16 +212,21 @@ public class MemcachedBackupSessionManager extends ManagerBase implements Lifecy
         ((MemcachedBackupSession)session).setSessionIdChanged(true);
     }
 
+    @Override
+    public boolean isMaxInactiveIntervalSet() {
+        return _maxInactiveInterval != null;
+    }
+
     public int getMaxInactiveInterval() {
         return _maxInactiveInterval;
     }
 
     public void setMaxInactiveInterval(int interval) {
-        int oldMaxInactiveInterval = _maxInactiveInterval;
+        Integer oldMaxInactiveInterval = _maxInactiveInterval;
         _maxInactiveInterval = interval;
         support.firePropertyChange("maxInactiveInterval",
-                Integer.valueOf(oldMaxInactiveInterval),
-                Integer.valueOf(_maxInactiveInterval));
+                oldMaxInactiveInterval,
+                _maxInactiveInterval);
     }
 
     /**